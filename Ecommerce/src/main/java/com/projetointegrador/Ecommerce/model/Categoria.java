89421d1d5f0e7fd7e7fb8e547d4f33b525f68fdb
ricardotmendes <ricardo.cau.mendes@gmail.com>
2020-10-05 15:40:49 -0300

    model.Categoria(corrigido)

--- Ecommerce/src/main/java/com/projetointegrador/Ecommerce/model/Categoria.java
+++ Ecommerce/src/main/java/com/projetointegrador/Ecommerce/model/Categoria.java
@@ -5,9 +5,10 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
-import com.sun.istack.NotNull;
+;
 
 @Entity
 @Table(name = "tb_Categoria")
