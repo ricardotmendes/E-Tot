ebb7450a9d497a5ead57e3e9d202a6ecf0d455c1
ricardotmendes <ricardo.cau.mendes@gmail.com>
2020-10-05 15:26:11 -0300

    Categoria criação

---
+++ Ecommerce/src/main/java/com/projetointegrador/Ecommerce/model/Categoria.java
@@ -0,0 +1,61 @@
+package com.projetointegrador.Ecommerce.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.Size;
+
+import com.sun.istack.NotNull;
+
+@Entity
+@Table(name = "tb_Categoria")
+public class Categoria {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long idCategoria;
+	
+	@NotNull
+	@Size(min = 2, max = 255)
+	private String NomeCategoria;
+	
+	@NotNull
+	@Size(min = 2, max = 255)
+	private String TipoCategoria;
+	
+	private boolean PresencialEAD;
+
+	public long getIdCategoria() {
+		return idCategoria;
+	}
+
+	public void setIdCategoria(long idCategoria) {
+		this.idCategoria = idCategoria;
+	}
+
+	public String getNomeCategoria() {
+		return NomeCategoria;
+	}
+
+	public void setNomeCategoria(String nomeCategoria) {
+		NomeCategoria = nomeCategoria;
+	}
+
+	public String getTipoCategoria() {
+		return TipoCategoria;
+	}
+
+	public void setTipoCategoria(String tipoCategoria) {
+		TipoCategoria = tipoCategoria;
+	}
+
+	public boolean isPresencialEAD() {
+		return PresencialEAD;
+	}
+
+	public void setPresencialEAD(boolean presencialEAD) {
+		PresencialEAD = presencialEAD;
+	}
+	
+}
